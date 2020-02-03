457f92e1286bd3141287d28f1c513a2d59710f24
Ulrika Bremberg <ulrikabremberg@student-220-80.eduroam.uu.se>
2020-02-03 13:36:26 +0100

    created burger constructor

---
+++ public/js/js_script.js
@@ -0,0 +1,24 @@
+
+
+function Burger(n, a, k, i){
+  this.name = n;
+  this.allergy = a;
+  this.kcal = k;
+  this.image = i;
+
+  this.item = function(){
+    return this.name + ' ' + this.kcal;
+  };
+}
+
+let Halloumi = new Burger("Hallo(umi) I like your style", "Gluten and Milk", "760 kcal", "");
+
+console.log(Halloumi.item());
+
+let Chickpea = new Burger("Where the Chick(pea)s at?", "Gluten", "700 kcal", "");
+
+console.log(Chickpea.item());
+
+let Bean = new Burger("Bean out lately?", "Gluten", "800 kcal", "");
+
+console.log(Bean.item());
