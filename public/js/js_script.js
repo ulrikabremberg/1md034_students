a57c5a786f334946ec6c6c2eeddf8e81bd2a5df6
Ulrika Bremberg <ulrikabremberg@Ulrikas-MBP.lan>
2020-02-04 11:44:10 +0100

    Text in script instead of html

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -13,20 +13,34 @@ function Burger(n, a, k, i){
 
 let Halloumi = new Burger("Hallo(umi) I like your style", "Gluten and Milk", "760 kcal", "");
 
-console.log(Halloumi.item());
-
 let Chickpea = new Burger("Where the Chick(pea)s at?", "Gluten", "700 kcal", "");
-
-console.log(Chickpea.item());
 
 let Bean = new Burger("Bean out lately?", "Gluten", "800 kcal", "");
 
-console.log(Bean.item());
-
 let LivsBurger = new Burger("Liv's burger", "Fish", "670 kcal", "");
-
-console.log(LivsBurger.item());
 
 let Mystery = new Burger("Mystery Burger", "?", "? kcal", "");
 
-console.log(Mystery.item());
+
+let ID = document.getElementById("myID");
+
+let halloumi = document.createElement('h4');
+halloumi.innerHTML = Halloumi.item();
+ID.appendChild(halloumi);
+
+
+let chickpea = document.createElement('h4');
+chickpea.innerHTML = Chickpea.item();
+ID.appendChild(chickpea);
+
+let bean = document.createElement('h4');
+bean.innerHTML = Bean.item();
+ID.appendChild(bean);
+
+let mystery = document.createElement('h4');
+mystery.innerHTML = Mystery.item();
+ID.appendChild(mystery);
+
+let liv = document.createElement('h4');
+liv.innerHTML = LivsBurger.item();
+ID.appendChild(liv);
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,6 +1,36 @@
-const vm = new Vue({
+function Burger(n, a, k, i) {
+  this.name = n;
+  this.allergy = a;
+  this.kcal = k;
+  this.image = i;
+
+  this.item = function() {
+    return this.name + ' ' + this.kcal;
+  };
+}
+
+let Halloumi = new Burger("Hallo(umi) I like your style", "Gluten and Milk", "760 kcal", "");
+
+let Chickpea = new Burger("Where the Chick(pea)s at?", "Gluten", "700 kcal", "");
+
+let Bean = new Burger("Bean out lately?", "Gluten", "800 kcal", "");
+
+let LivsBurger = new Burger("Liv's burger", "Fish", "670 kcal", "");
+
+let Mystery = new Burger("Mystery Burger", "?", "? kcal", "");
+
+
+const heading = new Vue({
   el: '#myID',
   data: {
     arbitraryVariableName: 'Välj en burgare'
   }
+})
+
+const burgers = new Vue({
+  el: '#burgerID',
+  data: {
+    burgerList: Halloumi.item() + '\n' + Chickpea.item() + '\n' + Bean.item() + '\n' + Mystery.item() + '\n' + LivsBurger.item()
+  }
+
 })
