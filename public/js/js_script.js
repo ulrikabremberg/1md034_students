5cab9e28dcbdac8efde4e373586b22d1c6ea270d
Ulrika Bremberg <ulrikabremberg@Ulrikas-MBP.lan>
2020-02-11 12:02:54 +0100

    Missed to commit javascript earlier, but now not compatible with html though since I started with Vue

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -9,18 +9,20 @@ function Burger(n, a, k, i) {
   };
 }
 
-let Halloumi = new Burger("Hallo(umi) I like your style", "Gluten and Milk", "760 kcal", "");
+let Halloumi = new Burger("Hallo(umi) I like your style", "Gluten and Milk", "760 kcal", "https://cdn.pixabay.com/photo/2014/05/27/11/31/hamburger-355342_1280.jpg");
 
-let Chickpea = new Burger("Where the Chick(pea)s at?", "Gluten", "700 kcal", "");
+let Chickpea = new Burger("Where the Chick(pea)s at?", "Gluten", "700 kcal", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROQc_T-KlFyJ3nh0EHab2SusfwshUMNaLW2VcrR_0uIjqDA3b0aQ&s");
 
-let Bean = new Burger("Bean out lately?", "Gluten", "800 kcal", "");
+let Bean = new Burger("Bean out lately?", "Gluten", "800 kcal", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwkqwu7BYs4eO8nmUG0KDy-McHlxfA1OMIBZgrjuukfIxku8Ty&s");
 
 let LivsBurger = new Burger("Liv's burger", "Fish", "670 kcal", "");
 
 let Mystery = new Burger("Mystery Burger", "", "? kcal", "");
 
 
-let ID = document.getElementById("myID");
+/*let ID = document.getElementById("myID");
+
+//let A = document.getElementsByClassName('burger a');
 
 let menu = [Halloumi, Chickpea, Bean, LivsBurger, Mystery];
 
@@ -34,4 +36,73 @@ for (let i = 0; i < menu.length; i++) {
     allergies.innerHTML = menu[i].allergy;
     ID.appendChild(allergies);
   }
-}
+}*/
+
+let burgerA = document.getElementById("burger a");
+
+let aName = document.createElement('h3');
+aName.innerHTML = Halloumi.name;
+
+let aImage = document.createElement('img');
+aImage.src = Halloumi.image;
+aImage.width = "300";
+
+let aInfo = document.createElement('ul');
+let aKcal = document.createElement('li');
+aKcal.innerHTML = Halloumi.kcal;
+let aAllergy = document.createElement('li');
+aAllergy.innerHTML = Halloumi.allergy;
+
+aInfo.appendChild(aKcal);
+aInfo.appendChild(aAllergy);
+
+burgerA.appendChild(aName);
+burgerA.appendChild(aImage);
+burgerA.appendChild(aInfo);
+
+
+//ChickPea
+let burgerB = document.getElementById("burger b");
+
+let bName = document.createElement('h3');
+bName.innerHTML = Chickpea.name;
+
+let bImage = document.createElement('img');
+bImage.src = Chickpea.image;
+bImage.width = "300";
+
+let bInfo = document.createElement('ul');
+let bKcal = document.createElement('li');
+bKcal.innerHTML = Chickpea.kcal;
+let bAllergy = document.createElement('li');
+bAllergy.innerHTML = Chickpea.allergy;
+
+bInfo.appendChild(bKcal);
+bInfo.appendChild(bAllergy);
+
+burgerB.appendChild(bName);
+burgerB.appendChild(bImage);
+burgerB.appendChild(bInfo);
+
+//ChickPea
+let burgerC = document.getElementById("burger c");
+
+let cName = document.createElement('h3');
+cName.innerHTML = Bean.name;
+
+let cImage = document.createElement('img');
+cImage.src = Bean.image;
+cImage.width = "300";
+
+let cInfo = document.createElement('ul');
+let cKcal = document.createElement('li');
+cKcal.innerHTML = Bean.kcal;
+let cAllergy = document.createElement('li');
+cAllergy.innerHTML = Bean.allergy;
+
+cInfo.appendChild(cKcal);
+cInfo.appendChild(cAllergy);
+
+burgerC.appendChild(cName);
+burgerC.appendChild(cImage);
+burgerC.appendChild(cInfo);
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,35 +1,50 @@
-function Burger(n, a, k, i) {
-  this.name = n;
-  this.allergy = a;
-  this.kcal = k;
-  this.image = i;
+window.onload = function(){
 
-  this.item = function() {
-    return this.name + ' ' + this.kcal;
-  };
-}
+  function Burger(n, a, k, i) {
+    this.name = n;
+    this.allergy = a;
+    this.kcal = k;
+    this.image = i;
 
-let Halloumi = new Burger("Hallo(umi) I like your style", "Gluten and Milk", "760 kcal", "");
-
-let Chickpea = new Burger("Where the Chick(pea)s at?", "Gluten", "700 kcal", "");
-
-let Bean = new Burger("Bean out lately?", "Gluten", "800 kcal", "");
-
-let LivsBurger = new Burger("Liv's burger", "Fish", "670 kcal", "");
-
-let Mystery = new Burger("Mystery Burger", "?", "? kcal", "");
-
-
-
-
-const heading = new Vue({
-  el: '#myID',
-  data: {
-    arbitraryVariableName: 'Välj en burgare',
-    menu: [Halloumi.item(), Chickpea.item(), Bean.item(), LivsBurger.item(), Mystery.item()]
+    this.item = function() {
+      return this.name + ' ' + this.kcal;
+    };
   }
-})
 
+  let Halloumi = new Burger("Hallo(umi) I like your style", "Gluten and Milk", "760 kcal", "");
+
+  let Chickpea = new Burger("Where the Chick(pea)s at?", "Gluten", "700 kcal", "");
+
+  let Bean = new Burger("Bean out lately?", "Gluten", "800 kcal", "");
+
+  let LivsBurger = new Burger("Liv's burger", "Fish", "670 kcal", "");
+
+  let Mystery = new Burger("Mystery Burger", "?", "? kcal", "");
+
+
+
+
+  const first = new Vue({
+    el: '#burger a',
+    data: {
+      name1: Halloumi.name,
+      img1: Halloumi.image,
+      kcal1: Halloumi.kcal,
+      allergy1: Halloumi.allergy,
+    }
+  })
+
+  const second = new Vue({
+    el: '#burger b',
+    data: {
+      name2: Chickpea.name,
+      img2: Chickpea.image,
+      kcal2: Chickpea.kcal,
+      allergy2: Chickpea.allergy,
+    }
+  })
+
+}
 
 /* Html för detta är : <script src="https://vuejs.org/js/vue.js"></script>
 <div id="myID" style="white-space:pre-wrap">
